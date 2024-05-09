9d57522e2711355a69bc705bbc4c0e656e057ce9
tsp0305 <162017015+tsp0305@users.noreply.github.com>
2024-05-10 02:42:19 +0530

    Update customer_page.java with start ride

--- customer_page.java
+++ customer_page.java
@@ -5,15 +5,28 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
 import javax.swing.JLabel;
+import java.awt.BorderLayout;
 import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class customer_page extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPasswordField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -36,63 +49,103 @@ public class customer_page extends JFrame {
 	 */
 	public customer_page() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 709, 392);
+		setBounds(0, 0, 1500, 1000);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("  Customer Details");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel.setBounds(242, 11, 176, 45);
-		contentPane.add(lblNewLabel);
-		
-		JButton btnNewButton_1 = new JButton("   Recharge");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 17));
-		btnNewButton_1.setBounds(84, 67, 176, 52);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_1_1 = new JButton("   Add Tag");
-		btnNewButton_1_1.setFont(new Font("Tahoma", Font.BOLD, 17));
-		btnNewButton_1_1.setBounds(84, 157, 182, 45);
-		contentPane.add(btnNewButton_1_1);
-		
-		JButton btnNewButton_1_2 = new JButton("   Recharge History");
-		btnNewButton_1_2.setFont(new Font("Tahoma", Font.BOLD, 17));
-		btnNewButton_1_2.setBounds(84, 231, 187, 52);
-		contentPane.add(btnNewButton_1_2);
-		
 		JPanel panel = new JPanel();
-		panel.setBounds(376, 61, 307, 255);
+		panel.setBackground(new Color(14,132,248));
+		panel.setBounds(0, 0, 1370, 40);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("  Name :");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1.setBounds(21, 11, 77, 35);
-		panel.add(lblNewLabel_1);
+		JLabel lblNewLabel_3 = new JLabel("SDP");
+		lblNewLabel_3.setFont(new Font("Gadugi", Font.BOLD, 17));
+		lblNewLabel_3.setBounds(10, 5, 104, 35);
+		panel.add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Balance : ");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1_1.setBounds(31, 81, 77, 35);
-		panel.add(lblNewLabel_1_1);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(14,132,248));
+		panel_1.setBounds(-93, 423, 1463, 315);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
 		
-		JLabel lblNewLabel_1_2 = new JLabel("Acc No : ");
-		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1_2.setBounds(31, 150, 77, 35);
-		panel.add(lblNewLabel_1_2);
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon("D:\\photo\\i11.png"));
+		lblNewLabel_5.setBounds(128, 21, 230, 230);
+		panel_1.add(lblNewLabel_5);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(130, 11, 122, 35);
-		panel.add(textPane);
+		JButton btnNewButton = new JButton("Recharge");
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 19));
+		btnNewButton.setBounds(181, 262, 144, 42);
+		panel_1.add(btnNewButton);
 		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setBounds(130, 81, 122, 35);
-		panel.add(textPane_1);
+		JLabel lblNewLabel_5_1 = new JLabel("");
+		lblNewLabel_5_1.setIcon(new ImageIcon("D:\\photo\\i4.png"));
+		lblNewLabel_5_1.setBounds(434, 32, 196, 219);
+		panel_1.add(lblNewLabel_5_1);
 		
-		JTextPane textPane_1_1 = new JTextPane();
-		textPane_1_1.setBounds(130, 150, 122, 35);
-		panel.add(textPane_1_1);
+		JButton btnAddTag = new JButton("Add Tag");
+		btnAddTag.setFont(new Font("Tahoma", Font.BOLD, 19));
+		btnAddTag.setBackground(Color.WHITE);
+		btnAddTag.setBounds(467, 262, 144, 42);
+		panel_1.add(btnAddTag);
+		
+		JLabel lblNewLabel_5_1_1 = new JLabel("");
+		lblNewLabel_5_1_1.setIcon(new ImageIcon("D:\\photo\\i3.png"));
+		lblNewLabel_5_1_1.setBounds(705, 32, 196, 219);
+		panel_1.add(lblNewLabel_5_1_1);
+		
+		JButton btnPaymentHistory = new JButton("Payment History");
+		btnPaymentHistory.setFont(new Font("Tahoma", Font.BOLD, 19));
+		btnPaymentHistory.setBackground(Color.WHITE);
+		btnPaymentHistory.setBounds(705, 262, 202, 42);
+		panel_1.add(btnPaymentHistory);
+		
+		JLabel lblNewLabel_5_1_1_1 = new JLabel("");
+		lblNewLabel_5_1_1_1.setIcon(new ImageIcon("D:\\photo\\i2.png"));
+		lblNewLabel_5_1_1_1.setBounds(971, 32, 196, 219);
+		panel_1.add(lblNewLabel_5_1_1_1);
+		
+		JButton btnStartDriving = new JButton("Start Driving");
+		btnStartDriving.setFont(new Font("Tahoma", Font.BOLD, 19));
+		btnStartDriving.setBackground(Color.WHITE);
+		btnStartDriving.setBounds(965, 262, 202, 42);
+		panel_1.add(btnStartDriving);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("D:\\photo\\i8.png"));
+		lblNewLabel.setBounds(423, 138, 61, 66);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("D:\\photo\\i9.png"));
+		lblNewLabel_1.setBounds(75, 91, 320, 315);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon("D:\\photo\\i10.png"));
+		lblNewLabel_2.setBounds(776, 138, 61, 66);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("      Name");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_4.setBounds(507, 149, 178, 40);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("        Balance");
+		lblNewLabel_4_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_4_1.setBounds(890, 149, 178, 40);
+		contentPane.add(lblNewLabel_4_1);
+		
+		JLabel lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setBounds(790, 253, 61, 66);
+		contentPane.add(lblNewLabel_6);
+		
 	}
 }
