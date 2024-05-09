38829790939dc39a2df0d0fb67f38dfeb047e04c
tsp0305 <162017015+tsp0305@users.noreply.github.com>
2024-05-10 01:25:43 +0530

    Update customer.java with alignment

--- customer.java
+++ customer.java
@@ -15,6 +15,9 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 
 public class customer extends JFrame {
 
@@ -56,83 +59,93 @@ public class customer extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(157, 0, 157));
+		panel.setBackground(new Color(14,132,248));
 		panel.setBounds(0, 0, 1370, 40);
 		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("SDP");
+		lblNewLabel_3.setFont(new Font("Gadugi", Font.BOLD, 17));
+		lblNewLabel_3.setBounds(10, 5, 104, 35);
+		panel.add(lblNewLabel_3);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(157, 0, 157));
-		panel_1.setBounds(0, 557, 1370, 192);
+		panel_1.setBackground(new Color(14,132,248));
+		panel_1.setBounds(0, 604, 1370, 145);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Register");
-		btnNewButton.setBackground(new Color(255, 255, 255));
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnNewButton.setBounds(620, 11, 120, 36);
-		panel_1.add(btnNewButton);
-		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(138, 0, 138), new Color(138, 0, 138), new Color(138, 0, 138), new Color(138, 0, 138)));
+		panel_2.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(198, 250, 253), new Color(14, 132, 248), new Color(14, 132, 248), new Color(14, 132, 248)));
 		panel_2.setBackground(new Color(255, 255, 255));
-		panel_2.setBounds(474, 51, 438, 573);
+		panel_2.setBounds(474, 51, 438, 553);
 		contentPane.add(panel_2);
 		panel_2.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("   SignUp");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewLabel.setBounds(147, 11, 111, 37);
+		lblNewLabel.setBounds(156, 11, 111, 37);
 		panel_2.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Name ");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1.setBounds(56, 68, 101, 29);
+		lblNewLabel_1.setBounds(79, 68, 101, 29);
 		panel_2.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Aadhaar No. ");
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1_1.setBounds(56, 148, 111, 29);
+		lblNewLabel_1_1.setBounds(79, 148, 111, 29);
 		panel_2.add(lblNewLabel_1_1);
 		
 		JLabel lblNewLabel_1_2 = new JLabel("E-mail");
 		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1_2.setBounds(56, 230, 101, 29);
+		lblNewLabel_1_2.setBounds(79, 230, 101, 29);
 		panel_2.add(lblNewLabel_1_2);
 		
 		JLabel lblNewLabel_1_3 = new JLabel("Password");
 		lblNewLabel_1_3.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1_3.setBounds(56, 310, 101, 29);
+		lblNewLabel_1_3.setBounds(79, 310, 101, 29);
 		panel_2.add(lblNewLabel_1_3);
 		
 		JLabel lblNewLabel_1_4 = new JLabel("Phone No.");
 		lblNewLabel_1_4.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1_4.setBounds(56, 387, 101, 29);
+		lblNewLabel_1_4.setBounds(79, 387, 101, 29);
 		panel_2.add(lblNewLabel_1_4);
 		
 		textField = new JTextField();
-		textField.setBounds(56, 108, 273, 29);
+		textField.setBounds(79, 108, 273, 29);
 		panel_2.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(56, 190, 273, 29);
+		textField_1.setBounds(79, 190, 273, 29);
 		panel_2.add(textField_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(56, 270, 273, 29);
+		textField_2.setBounds(79, 270, 273, 29);
 		panel_2.add(textField_2);
 		
 		textField_3 = new JPasswordField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(56, 347, 273, 29);
+		textField_3.setBounds(79, 347, 273, 29);
 		panel_2.add(textField_3);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
-		textField_4.setBounds(56, 427, 271, 29);
+		textField_4.setBounds(79, 427, 271, 29);
 		panel_2.add(textField_4);
+		
+		JButton btnNewButton = new JButton("Register");
+		btnNewButton.setBounds(138, 489, 120, 36);
+		panel_2.add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
 		
 	}
 }
