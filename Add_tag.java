1cae02e92ccfeb337ba86e2ab62e6fef4648346b
tsp0305 <162017015+tsp0305@users.noreply.github.com>
2024-05-10 13:52:51 +0530

    Update Add_tag.java with drop down

--- Add_tag.java
+++ Add_tag.java
@@ -5,11 +5,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Window;
+
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 
 public class Add_tag extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField textField_3;
+	private JTextField textField_4;
+	private JComboBox dropdown;
 
 	/**
 	 * Launch the application.
@@ -29,14 +49,94 @@ public class Add_tag extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @param comboBox 
 	 */
-	public Add_tag() {
+	public Add_tag(Window comboBox) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, 1500, 1000);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(14,132,248));
+		panel.setBounds(0, 0, 1370, 40);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("DPPS");
+		lblNewLabel_3.setFont(new Font("Gadugi", Font.BOLD, 17));
+		lblNewLabel_3.setBounds(10, 5, 104, 35);
+		panel.add(lblNewLabel_3);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(14,132,248));
+		panel_1.setBounds(0, 525, 1370, 224);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(198, 250, 253), new Color(14, 132, 248), new Color(14, 132, 248), new Color(14, 132, 248)));
+		panel_2.setBackground(new Color(255, 255, 255));
+		panel_2.setBounds(470, 79, 430, 439);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Add Tag");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setBounds(156, 30, 143, 37);
+		panel_2.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel(" Vehicle No.");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(79, 79, 101, 29);
+		panel_2.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel(" Vehicle Type");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1_1.setBounds(79, 174, 111, 29);
+		panel_2.add(lblNewLabel_1_1);
+		
+		panel_2.add(dropdown);
+		panel_2.add(textField_1);
+		
+		
+		
+		
+		textField = new JTextField();
+		textField.setBounds(79, 119, 273, 29);
+		panel_2.add(textField);
+		textField.setColumns(10);
+		
+		
+		
+		
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.setBounds(156, 317, 120, 36);
+		panel_2.add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		//JComboBox comboBox = new JComboBox();
+		String[] items = {"HCM", "LMV", "HCM", "2AXLE", "Ambulance"};
+		dropdown = new JComboBox<>(items);
+		dropdown.setBackground(new Color(255, 255, 255));
+        dropdown.setSelectedIndex(0);
+        
+		dropdown.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		dropdown.setBounds(79, 223, 273, 37);
+		panel_2.add(dropdown);
+		
 	}
-
 }
