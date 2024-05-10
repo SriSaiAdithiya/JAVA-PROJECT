d5cca26b1a315f9fcc476da6d5f4111f7b43ec8c
tsp0305 <162017015+tsp0305@users.noreply.github.com>
2024-05-10 12:08:36 +0530

    Update customer_page.java

--- customer_page.java
+++ customer_page.java
@@ -63,89 +63,125 @@ public class customer_page extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel_3 = new JLabel("SDP");
+		JLabel lblNewLabel_3 = new JLabel("DPPS");
 		lblNewLabel_3.setFont(new Font("Gadugi", Font.BOLD, 17));
 		lblNewLabel_3.setBounds(10, 5, 104, 35);
 		panel.add(lblNewLabel_3);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(14,132,248));
-		panel_1.setBounds(-93, 423, 1463, 315);
+		panel_1.setBounds(0, 446, 1370, 315);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
-		
-		JLabel lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setIcon(new ImageIcon("D:\\photo\\i11.png"));
-		lblNewLabel_5.setBounds(128, 21, 230, 230);
-		panel_1.add(lblNewLabel_5);
 		
 		JButton btnNewButton = new JButton("Recharge");
 		btnNewButton.setBackground(new Color(255, 255, 255));
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 19));
-		btnNewButton.setBounds(181, 262, 144, 42);
+		btnNewButton.setBounds(61, 224, 144, 42);
 		panel_1.add(btnNewButton);
 		
 		JLabel lblNewLabel_5_1 = new JLabel("");
 		lblNewLabel_5_1.setIcon(new ImageIcon("D:\\photo\\i4.png"));
-		lblNewLabel_5_1.setBounds(434, 32, 196, 219);
+		lblNewLabel_5_1.setBounds(293, 34, 196, 179);
 		panel_1.add(lblNewLabel_5_1);
 		
 		JButton btnAddTag = new JButton("Add Tag");
 		btnAddTag.setFont(new Font("Tahoma", Font.BOLD, 19));
 		btnAddTag.setBackground(Color.WHITE);
-		btnAddTag.setBounds(467, 262, 144, 42);
+		btnAddTag.setBounds(314, 224, 144, 42);
 		panel_1.add(btnAddTag);
 		
 		JLabel lblNewLabel_5_1_1 = new JLabel("");
 		lblNewLabel_5_1_1.setIcon(new ImageIcon("D:\\photo\\i3.png"));
-		lblNewLabel_5_1_1.setBounds(705, 32, 196, 219);
+		lblNewLabel_5_1_1.setBounds(583, 34, 196, 198);
 		panel_1.add(lblNewLabel_5_1_1);
 		
 		JButton btnPaymentHistory = new JButton("Payment History");
 		btnPaymentHistory.setFont(new Font("Tahoma", Font.BOLD, 19));
 		btnPaymentHistory.setBackground(Color.WHITE);
-		btnPaymentHistory.setBounds(705, 262, 202, 42);
+		btnPaymentHistory.setBounds(570, 224, 202, 42);
 		panel_1.add(btnPaymentHistory);
 		
 		JLabel lblNewLabel_5_1_1_1 = new JLabel("");
 		lblNewLabel_5_1_1_1.setIcon(new ImageIcon("D:\\photo\\i2.png"));
-		lblNewLabel_5_1_1_1.setBounds(971, 32, 196, 219);
+		lblNewLabel_5_1_1_1.setBounds(852, 11, 196, 200);
 		panel_1.add(lblNewLabel_5_1_1_1);
 		
 		JButton btnStartDriving = new JButton("Start Driving");
 		btnStartDriving.setFont(new Font("Tahoma", Font.BOLD, 19));
 		btnStartDriving.setBackground(Color.WHITE);
-		btnStartDriving.setBounds(965, 262, 202, 42);
+		btnStartDriving.setBounds(846, 224, 202, 42);
 		panel_1.add(btnStartDriving);
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon("D:\\photo\\i11.png"));
+		lblNewLabel_5.setBounds(72, 50, 232, 144);
+		panel_1.add(lblNewLabel_5);
+		
+		JButton btnRechargeHistory = new JButton("Recharge History");
+		btnRechargeHistory.setFont(new Font("Tahoma", Font.BOLD, 19));
+		btnRechargeHistory.setBackground(Color.WHITE);
+		btnRechargeHistory.setBounds(1139, 224, 202, 42);
+		panel_1.add(btnRechargeHistory);
+		
+		JLabel lblNewLabel_5_1_1_2 = new JLabel("");
+		lblNewLabel_5_1_1_2.setIcon(new ImageIcon("D:\\photo\\i3.png"));
+		lblNewLabel_5_1_1_2.setBounds(1145, 23, 196, 198);
+		panel_1.add(lblNewLabel_5_1_1_2);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("D:\\photo\\i8.png"));
-		lblNewLabel.setBounds(423, 138, 61, 66);
+		lblNewLabel.setBounds(419, 159, 74, 66);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setIcon(new ImageIcon("D:\\photo\\i9.png"));
-		lblNewLabel_1.setBounds(75, 91, 320, 315);
+		lblNewLabel_1.setBounds(74, 110, 320, 315);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon("D:\\photo\\i10.png"));
-		lblNewLabel_2.setBounds(776, 138, 61, 66);
+		lblNewLabel_2.setBounds(776, 159, 61, 66);
 		contentPane.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_4 = new JLabel("      Name");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_4.setBounds(507, 149, 178, 40);
+		lblNewLabel_4.setBounds(503, 174, 178, 40);
 		contentPane.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_4_1 = new JLabel("        Balance");
 		lblNewLabel_4_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_4_1.setBounds(890, 149, 178, 40);
+		lblNewLabel_4_1.setBounds(891, 174, 178, 40);
 		contentPane.add(lblNewLabel_4_1);
 		
 		JLabel lblNewLabel_6 = new JLabel("");
 		lblNewLabel_6.setBounds(790, 253, 61, 66);
 		contentPane.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("     Profile");
+		lblNewLabel_7.setFont(new Font("Verdana", Font.BOLD, 25));
+		lblNewLabel_7.setBounds(96, 72, 246, 40);
+		contentPane.add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon("D:\\photo\\email_3178165.png"));
+		lblNewLabel_8.setBounds(419, 266, 83, 54);
+		contentPane.add(lblNewLabel_8);
+		
+		JLabel lblNewLabel_4_2 = new JLabel("    E-mail");
+		lblNewLabel_4_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_4_2.setBounds(503, 279, 178, 40);
+		contentPane.add(lblNewLabel_4_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("");
+		lblNewLabel_2_1.setIcon(new ImageIcon("D:\\photo\\phone-call_455705.png"));
+		lblNewLabel_2_1.setBounds(776, 266, 61, 66);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_4_1_1 = new JLabel("       Phone");
+		lblNewLabel_4_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_4_1_1.setBounds(891, 279, 178, 40);
+		contentPane.add(lblNewLabel_4_1_1);
 		
 	}
 }
