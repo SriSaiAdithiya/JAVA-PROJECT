501f597f7abc4501855ce47b1e52ce59bcaf4698
tsp0305 <162017015+tsp0305@users.noreply.github.com>
2024-05-20 19:06:14 +0530

    Update customer_page.java

--- customer_page.java
+++ customer_page.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.border.SoftBevelBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.JLabel;
@@ -21,11 +22,14 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JTextPane;
 import javax.swing.JTextArea;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class customer_page extends JFrame {
 
@@ -37,6 +41,7 @@ public class customer_page extends JFrame {
 	private static String name;
 	private static String mail;
 	private static String phone;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -111,11 +116,13 @@ public class customer_page extends JFrame {
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(14,132,248));
-		panel_1.setBounds(0, 434, 1336, 315);
+		panel_1.setBounds(-11, 573, 1381, 176);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Recharge");
+		btnNewButton.setBounds(80, 64, 144, 42);
+		panel_1.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				 new Recharge_design(cid).setVisible(true);
@@ -123,115 +130,138 @@ public class customer_page extends JFrame {
 		});
 		btnNewButton.setBackground(new Color(255, 255, 255));
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 19));
-		btnNewButton.setBounds(61, 224, 144, 42);
-		panel_1.add(btnNewButton);
-		
-		JLabel lblNewLabel_5_1 = new JLabel("");
-		lblNewLabel_5_1.setIcon(new ImageIcon("D:\\photo\\i4.png"));
-		lblNewLabel_5_1.setBounds(293, 34, 196, 179);
-		panel_1.add(lblNewLabel_5_1);
 		
 		JButton btnAddTag = new JButton("Add Tag");
+		btnAddTag.setBounds(322, 64, 144, 42);
+		panel_1.add(btnAddTag);
 		btnAddTag.setFont(new Font("Tahoma", Font.BOLD, 19));
 		btnAddTag.setBackground(Color.WHITE);
-		btnAddTag.setBounds(314, 224, 144, 42);
-		panel_1.add(btnAddTag);
-		
-		JLabel lblNewLabel_5_1_1 = new JLabel("");
-		lblNewLabel_5_1_1.setIcon(new ImageIcon("D:\\photo\\i3.png"));
-		lblNewLabel_5_1_1.setBounds(583, 34, 196, 198);
-		panel_1.add(lblNewLabel_5_1_1);
 		
 		JButton btnPaymentHistory = new JButton("Payment History");
+		btnPaymentHistory.setBounds(541, 64, 202, 42);
+		panel_1.add(btnPaymentHistory);
 		btnPaymentHistory.setFont(new Font("Tahoma", Font.BOLD, 19));
 		btnPaymentHistory.setBackground(Color.WHITE);
-		btnPaymentHistory.setBounds(570, 224, 202, 42);
-		panel_1.add(btnPaymentHistory);
-		
-		JLabel lblNewLabel_5_1_1_1 = new JLabel("");
-		lblNewLabel_5_1_1_1.setIcon(new ImageIcon("D:\\photo\\i2.png"));
-		lblNewLabel_5_1_1_1.setBounds(852, 11, 196, 200);
-		panel_1.add(lblNewLabel_5_1_1_1);
 		
 		JButton btnStartDriving = new JButton("Start Driving");
+		btnStartDriving.setBounds(825, 64, 202, 42);
+		panel_1.add(btnStartDriving);
 		btnStartDriving.setFont(new Font("Tahoma", Font.BOLD, 19));
 		btnStartDriving.setBackground(Color.WHITE);
-		btnStartDriving.setBounds(846, 224, 202, 42);
-		panel_1.add(btnStartDriving);
-		
-		JLabel lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setIcon(new ImageIcon("D:\\photo\\i11.png"));
-		lblNewLabel_5.setBounds(62, 53, 232, 144);
-		panel_1.add(lblNewLabel_5);
 		
 		JButton btnRechargeHistory = new JButton("Recharge History");
+		btnRechargeHistory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+				 new Recharge_history(cid).setVisible(true);
+				
+			}
+		});
+		btnRechargeHistory.setBounds(1115, 64, 202, 42);
+		panel_1.add(btnRechargeHistory);
 		btnRechargeHistory.setFont(new Font("Tahoma", Font.BOLD, 19));
 		btnRechargeHistory.setBackground(Color.WHITE);
-		btnRechargeHistory.setBounds(1139, 224, 202, 42);
-		panel_1.add(btnRechargeHistory);
-		
-		JLabel lblNewLabel_5_1_1_2 = new JLabel("");
-		lblNewLabel_5_1_1_2.setIcon(new ImageIcon("D:\\photo\\i3.png"));
-		lblNewLabel_5_1_1_2.setBounds(1139, 34, 196, 198);
-		panel_1.add(lblNewLabel_5_1_1_2);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("D:\\photo\\i8.png"));
-		lblNewLabel.setBounds(419, 159, 74, 66);
+		lblNewLabel.setBounds(35, 173, 74, 66);
 		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("D:\\photo\\i9.png"));
-		lblNewLabel_1.setBounds(74, 110, 320, 315);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon("D:\\photo\\i10.png"));
-		lblNewLabel_2.setBounds(776, 159, 61, 66);
-		contentPane.add(lblNewLabel_2);
 		
 		
 		JLabel lblNewLabel_4 = new JLabel();
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_4.setBounds(503, 174, 178, 40);
+		lblNewLabel_4.setBounds(117, 188, 178, 40);
 		lblNewLabel_4.setText(name);
 		contentPane.add(lblNewLabel_4);
 		
-		JLabel lblNewLabel_4_1 = new JLabel("        Balance");
-		lblNewLabel_4_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_4_1.setBounds(891, 174, 178, 40);
-		contentPane.add(lblNewLabel_4_1);
-		
 		
 		JLabel lblNewLabel_6 = new JLabel("");
-		lblNewLabel_6.setBounds(790, 253, 61, 66);
+		lblNewLabel_6.setBounds(35, 385, 61, 66);
 		contentPane.add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_7 = new JLabel("     Profile");
 		lblNewLabel_7.setFont(new Font("Verdana", Font.BOLD, 25));
-		lblNewLabel_7.setBounds(96, 72, 246, 40);
+		lblNewLabel_7.setBounds(80, 108, 246, 40);
 		contentPane.add(lblNewLabel_7);
 		
 		JLabel lblNewLabel_8 = new JLabel();
 		lblNewLabel_8.setIcon(new ImageIcon("D:\\photo\\email_3178165.png"));
-		lblNewLabel_8.setBounds(419, 266, 83, 54);
+		lblNewLabel_8.setBounds(35, 291, 83, 54);
 		contentPane.add(lblNewLabel_8);
 		
 		JLabel lblNewLabel_4_2 = new JLabel(mail);
 		lblNewLabel_4_2.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_4_2.setBounds(503, 279, 178, 40);
+		lblNewLabel_4_2.setBounds(117, 301, 178, 40);
 		contentPane.add(lblNewLabel_4_2);
 		
 		JLabel lblNewLabel_2_1 = new JLabel();
 		lblNewLabel_2_1.setIcon(new ImageIcon("D:\\photo\\phone-call_455705.png"));
-		lblNewLabel_2_1.setBounds(776, 266, 61, 66);
+		lblNewLabel_2_1.setBounds(35, 413, 61, 66);
 		contentPane.add(lblNewLabel_2_1);
 		
 		JLabel lblNewLabel_4_1_1 = new JLabel(phone);
 		lblNewLabel_4_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_4_1_1.setBounds(891, 279, 178, 40);
+		lblNewLabel_4_1_1.setBounds(119, 427, 178, 40);
 		contentPane.add(lblNewLabel_4_1_1);
 		
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(null);
+		panel_2.setBackground(new Color(13, 13, 255));
+		panel_2.setBounds(0, 36, 1370, 40);
+		contentPane.add(panel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("Registered Vehicle ");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1.setBackground(new Color(192, 192, 192));
+		lblNewLabel_1.setBounds(768, 121, 203, 48);
+		contentPane.add(lblNewLabel_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(544, 173, 666, 320);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setBackground(new Color(192, 192, 192));
+		 table.setModel(new DefaultTableModel(
+	                new Object[][]{},
+	                new String[]{}
+	        ));
+		 loadVehicle(cid);
+		
 	}
-	}
+	
+private void loadVehicle(int cid) {
+    	
+        try (Connection con =  DBCONNECTIO.getConnection()) {
+            String query = "select vehicle.veh_type,vehicle.veh_no,tag.wallet_bal "
+            		+ "from tag join vehicle where vehicle.tag_id = tag.tag_id and vehicle.cid = ?;";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setInt(1, cid);
+            ResultSet rs = pst.executeQuery();
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int cols = rsmd.getColumnCount();
+            String[] colNames = new String[cols];
+            for (int i = 0; i < cols; i++) {
+                colNames[i] = rsmd.getColumnName(i + 1);
+            }
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            model.setColumnIdentifiers(colNames);
+
+            while (rs.next()) {
+                String[] row = new String[cols];
+                for (int i = 0; i < cols; i++) {
+                    row[i] = rs.getString(i + 1);
+                    System.out.println(row[i]) ;                   //System.out.println(i);
+                }
+                model.addRow(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+}
 
